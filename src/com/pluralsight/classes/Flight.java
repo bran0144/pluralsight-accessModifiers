fb471a5a9d4d12f37f411841130a65535b304c26
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-02-26 09:08:32 -0600

    using variable number of parameter values

--- src/com/pluralsight/classes/Flight.java
+++ src/com/pluralsight/classes/Flight.java
@@ -1,9 +1,10 @@
 package com.pluralsight.classes;
 
 public class Flight {
-    private int passengers, flightNumber, seats = 150;
+    private int passengers, flightNumber, totalCheckedBags, seats = 150;
     private char flightClass;
     private boolean[] isSeatAvailable;
+    private int maxCarryOns = seats * 2, totalCarryOns;
 
     {
         isSeatAvailable = new boolean[seats];
@@ -21,13 +22,28 @@ public class Flight {
     }
 
     public void add1Passenger() {
-        if(passengers < seats)
+        if(hasSeating())
             passengers +=1;
         else
             handleTooMany();
     }
+
+    public void add1Passenger(int bags){
+        if(hasSeating()) {
+            add1Passenger();
+            totalCheckedBags += bags;
+        }
+    }
+
+    private boolean hasSeating() {
+        return passengers < seats;
+    }
     private void handleTooMany() {
         System.out.println("Too many'");
+    }
+
+    private boolean hasCarryOnSpace(int carryOns) {
+        return totalCarryOns + carryOns <= maxCarryOns;
     }
 
     public boolean hasRoom(Flight f2) {
@@ -47,6 +63,15 @@ public class Flight {
     }
     public void setSeats(int seats) {
         this.seats = seats;
+    }
+    public void addPassengers(Passenger... list) {
+        if(hasSeating(list.length)) {
+            passengers += list.length;
+            for (Passenger passenger : list)
+                totalCheckedBags += passenger.getCheckedBags();
+        }
+        else
+            handleTooMany();
     }
     }
 
